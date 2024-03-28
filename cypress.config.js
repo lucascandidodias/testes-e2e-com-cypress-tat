f3b07dbc4ffdbe4a40d2376f2d61579157c759a4
Lucas Candido Dias <lucas.dias@meta.com.br>
2024-03-28 16:32:14 -0300

    Create e2e tests with cypress

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://notes-serverless-app.com',
+    defaultCommandTimeout: 10000,
+    responseTimeout: 10000,
+    requestTimeout: 10000,
+    chromeWebSecurity: false
+    // setupNodeEvents(on, config) {
+    //   // implement node event listeners here
+    // },
+  },
+})
