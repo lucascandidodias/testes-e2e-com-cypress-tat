39ffcb9a0002058dba0f8e31e4d6b318468c19bc
lucascandidodias <lucas_ita14@hotmail.com>
2024-03-30 10:56:17 -0300

    comentario teste

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -8,5 +8,6 @@ describe('Sign up', () => {
     cy.fillSignupFormAndSubmit(emailAddress, password)
     cy.contains('h1', 'Your Notes').should('be.visible')
     cy.contains('a', 'Create a new note').should('be.visible')
+    //teste
   })
 })
